4aa007846915e09fad57be51ce36b555f062f269
Aleksander Jacukowicz <aleksander.jacukowicz@gmail.com>
2020-05-17 21:29:09 +0200

    Added simple navigation to create add fragment

--- app/src/main/java/com/ajdigital/covid19_mask_stats_nav/fragments/MainFragment.kt
+++ app/src/main/java/com/ajdigital/covid19_mask_stats_nav/fragments/MainFragment.kt
@@ -5,10 +5,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
 
 import com.ajdigital.covid19_mask_stats_nav.R
+import kotlinx.android.synthetic.main.fragment_main.*
 
 class MainFragment : Fragment() {
+
+    lateinit var navController: NavController
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -18,4 +24,14 @@ class MainFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_main, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        navController = Navigation.findNavController(view)
+        add_record.setOnClickListener{
+            navController.navigate(R.id.action_mainFragment_to_addRecord)
+        }
+        upload_records.setOnClickListener{
+            Toast.makeText(activity,"Not implemented yet",Toast.LENGTH_SHORT).show()
+        }
+    }
 }
