23112c42aa9d26fbf6268345a8e26099c945e1f2
Aleksander Jacukowicz <aleksander.jacukowicz@gmail.com>
2020-05-17 21:18:12 +0200

    Prepared navigation fragments

---
+++ app/src/main/java/com/ajdigital/covid19_mask_stats_nav/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.ajdigital.covid19_mask_stats_nav
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/com/ajdigital/covid19_mask_stats_nav/fragments/MainFragment.kt
@@ -0,0 +1,21 @@
+package com.ajdigital.covid19_mask_stats_nav.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.ajdigital.covid19_mask_stats_nav.R
+
+class MainFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+}
