97a1961070ad662cdcc6886194b31a4d541c3e3e
Aleksander Jacukowicz <aleksander.jacukowicz@gmail.com>
2020-06-07 20:35:04 +0200

    Added Logging fragment

---
+++ app/src/main/java/com/ajdigital/covid19_mask_stats_nav/ui/login/Login.kt
@@ -0,0 +1,34 @@
+package com.ajdigital.covid19_mask_stats_nav.ui.login
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import com.ajdigital.covid19_mask_stats_nav.R
+import kotlinx.android.synthetic.main.fragment_login.*
+
+class Login : Fragment() {
+
+    private lateinit var navController: NavController
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_login, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        navController = Navigation.findNavController(view)
+        login_btn.setOnClickListener{
+            navController.navigate(R.id.action_login_to_mainFragment)
+        }
+
+
+    }
+}
